2d81d816d2ee9bbe1e9df0f7c78b2a3dab427d6e
A0n1x <pakshin.2018@gmail.com>
2024-05-13 20:07:55 +0700

    Updated .github/workflows/build.yml file.

---
+++ ci-cd/get_changelog.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <PATH_TO_FILE> <VERSION>"
+    exit 1
+fi
+
+file_path=$1
+version=$2
+
+if [ ! -f "$file_path" ]; then
+    echo "File not found: $file_path"
+    exit 1
+fi
+
+current_version=""
+
+while IFS= read -r line; do
+    if [[ "$line" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+        current_version="$line"
+    elif [ "$current_version" = "$version" ] && [ -n "$line" ]; then
+        echo "$line"
+    fi
+done < "$file_path"
