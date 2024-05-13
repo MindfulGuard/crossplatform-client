e3b8d56c3232646f34ceb68b706a49ba55356c71
A0n1x <pakshin.2018@gmail.com>
2024-05-13 10:36:31 +0700

    Fixed ci-cd/get_changelog.sh script

--- ci-cd/get_changelog.sh
+++ ci-cd/get_changelog.sh
@@ -5,18 +5,13 @@ if [ "$#" -ne 2 ]; then
     exit 1
 fi
 
-if [ ! -f "$1" ]; then
-    echo "Error: File $1 not found."
-    exit 1
-fi
+changelog_file=$1
+current_version=$2
 
-current_version="$2"
-
-latest_version=$(grep -E '^[0-9]+\.[0-9]+\.[0-9]+$' "$1" | head -n 1)
+latest_version=$(grep -E "^[0-9]+\.[0-9]+\.[0-9]+" "$changelog_file" | head -n 1)
 
 if [ "$current_version" = "$latest_version" ]; then
-    log=$(awk "/^$latest_version$/,/^([0-9]+\.[0-9]+\.[0-9]+)$/ {print}" "$1" | sed '$d')
-    echo "$log"
+    sed -n "/$latest_version/,/^[0-9]+\.[0-9]+\.[0-9]+/{p}" "$changelog_file"
 else
     echo ""
 fi
